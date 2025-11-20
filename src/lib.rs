204083e6c0ab9250e7bb3ae7a2150ecc606c0187
Julian Rosse <julian@helixbass.net>
2025-11-20 11:40:09 -0500

    working on schema

--- src/lib.rs
+++ src/lib.rs
@@ -32,15 +32,29 @@ impl Schema {
 }
 
 pub enum Type {
-    ObjectType(ObjectType),
+    Object(ObjectType),
+    Scalar(ScalarType),
 }
 
 impl Type {
     pub fn is_query_type(&self) -> bool {
         matches!(
             self,
-            Self::ObjectType(type_) if type_.is_query_type()
+            Self::Object(type_) if type_.is_query_type()
         )
+    }
+}
+
+pub trait TypeInterface {
+    fn name(&self) -> &str;
+}
+
+impl TypeInterface for Type {
+    fn name(&self) -> &str {
+        match self {
+            Self::Object(type_) => type_.name(),
+            Self::Scalar(type_) => type_.name(),
+        }
     }
 }
 
@@ -51,12 +65,77 @@ pub enum OperationType {
 }
 
 pub struct ObjectType {
+    name: String,
     pub is_top_level_type: Option<OperationType>,
+    pub fields: Vec<Field>,
 }
 
 impl ObjectType {
+    pub fn new(name: String, fields: Vec<Field>, is_top_level_type: Option<OperationType>) -> Self {
+        Self {
+            name,
+            fields,
+            is_top_level_type,
+        }
+    }
+
     pub fn is_query_type(&self) -> bool {
         matches!(self.is_top_level_type, Some(OperationType::Query))
+    }
+}
+
+impl TypeInterface for ObjectType {
+    fn name(&self) -> &str {
+        &self.name
+    }
+}
+
+pub enum ScalarType {
+    BuiltIn(BuiltInScalarType),
+}
+
+impl TypeInterface for ScalarType {
+    fn name(&self) -> &str {
+        match self {
+            Self::BuiltIn(type_) => type_.name(),
+        }
+    }
+}
+
+pub enum BuiltInScalarType {
+    String(StringType),
+}
+
+impl TypeInterface for BuiltInScalarType {
+    fn name(&self) -> &str {
+        match self {
+            Self::String(type_) => type_.name(),
+        }
+    }
+}
+
+pub struct StringType {}
+
+impl StringType {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl TypeInterface for StringType {
+    fn name(&self) -> &str {
+        "String"
+    }
+}
+
+pub struct Field {
+    pub name: String,
+    pub type_: Type,
+}
+
+impl Field {
+    pub fn new(name: String, type_: Type) -> Self {
+        Self { name, type_ }
     }
 }
 
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use sauvignon::{
+    BuiltInScalarType, Error as SauvignonError, Field, ObjectType, OperationType, ScalarType,
+    Schema, StringType, Type,
+};
+
+fn main() -> Result<(), SauvignonError> {
+    let actor_type = Type::Object(ObjectType::new(
+        "Actor".to_owned(),
+        vec![Field::new(
+            "name".to_owned(),
+            Type::Scalar(ScalarType::BuiltIn(BuiltInScalarType::String(
+                StringType::new(),
+            ))),
+        )],
+        None,
+    ));
+
+    let query_type = Type::Object(ObjectType::new(
+        "Query".to_owned(),
+        vec![Field::new("actor".to_owned(), actor_type)],
+        Some(OperationType::Query),
+    ));
+
+    let schema = Schema::try_new(vec![query_type])?;
+
+    Ok(())
+}
