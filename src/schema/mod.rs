0e98d53b242688261b98894f3559b4f1f163cbe9
Julian Rosse <julian@helixbass.net>
2025-11-22 08:23:26 -0500

    working on request

--- src/schema/mod.rs
+++ src/schema/mod.rs
@@ -2,9 +2,9 @@ use std::collections::HashMap;
 
 use crate::{
     builtin_types, fields_in_progress_new, CarverOrPopulator, Error, ExternalDependencyValues,
-    FieldsInProgress, InProgress, InProgressRecursing, IndexMap, InternalDependencyValues,
-    QueryPlan, Request, Response, ResponseValue, ResponseValueOrInProgress,
-    Result as SauvignonResult, Type, TypeInterface,
+    FieldPlan, FieldsInProgress, InProgress, InProgressRecursing, IndexMap,
+    InternalDependencyResolver, InternalDependencyValues, QueryPlan, Request, Response,
+    ResponseValue, ResponseValueOrInProgress, Result as SauvignonResult, Type, TypeInterface,
 };
 
 pub struct Schema {
@@ -33,7 +33,7 @@ impl Schema {
     }
 
     pub async fn request(&self, request: Request) -> Response {
-        let response = compute_response(self, &request);
+        let response = compute_response(self, &request).await;
         unimplemented!()
     }
 
@@ -49,12 +49,12 @@ impl Schema {
     }
 }
 
-fn compute_response(schema: &Schema, request: &Request) -> ResponseValue {
+async fn compute_response(schema: &Schema, request: &Request) -> ResponseValue {
     let query_plan = QueryPlan::new(&request, schema);
     let response_in_progress = query_plan.initial_response_in_progress();
     let mut fields_in_progress = response_in_progress.fields;
     loop {
-        let ret = progress_fields(fields_in_progress);
+        let ret = progress_fields(fields_in_progress).await;
         let is_done = ret.0;
         fields_in_progress = ret.1;
         if is_done {
@@ -63,7 +63,9 @@ fn compute_response(schema: &Schema, request: &Request) -> ResponseValue {
     }
 }
 
-fn progress_fields<'a>(fields_in_progress: FieldsInProgress<'a>) -> (bool, FieldsInProgress<'a>) {
+async fn progress_fields<'a>(
+    fields_in_progress: FieldsInProgress<'a>,
+) -> (bool, FieldsInProgress<'a>) {
     let is_done = fields_in_progress
         .values()
         .all(|field| matches!(field, ResponseValueOrInProgress::ResponseValue(_)));
@@ -71,64 +73,80 @@ fn progress_fields<'a>(fields_in_progress: FieldsInProgress<'a>) -> (bool, Field
         return (true, fields_in_progress);
     }
 
-    (
-        false,
-        IndexMap::from_iter(fields_in_progress.into_iter().map(
-            |(field_name, response_value_or_in_progress)| {
-                (
-                    field_name,
-                    match response_value_or_in_progress {
-                        ResponseValueOrInProgress::ResponseValue(response_value) => {
-                            ResponseValueOrInProgress::ResponseValue(response_value)
+    let mut progressed = IndexMap::new();
+    for (field_name, response_value_or_in_progress) in fields_in_progress {
+        progressed.insert(
+            field_name,
+            match response_value_or_in_progress {
+                ResponseValueOrInProgress::ResponseValue(response_value) => {
+                    ResponseValueOrInProgress::ResponseValue(response_value)
+                }
+                ResponseValueOrInProgress::InProgress(InProgress {
+                    field_plan,
+                    external_dependency_values,
+                }) => {
+                    let internal_dependency_values =
+                        populate_internal_dependencies(field_plan, &external_dependency_values)
+                            .await;
+                    match &field_plan.field_type.resolver.carver_or_populator {
+                        CarverOrPopulator::Populator(populator) => {
+                            let mut populated = ExternalDependencyValues::default();
+                            populator.populate(
+                                &mut populated,
+                                &external_dependency_values,
+                                &internal_dependency_values,
+                            );
+                            ResponseValueOrInProgress::InProgressRecursing(
+                                InProgressRecursing::new(
+                                    field_plan,
+                                    populated,
+                                    fields_in_progress_new(
+                                        field_plan.selection_set.as_ref().unwrap(),
+                                    ),
+                                ),
+                            )
                         }
-                        ResponseValueOrInProgress::InProgress(InProgress {
-                            field_plan,
-                            external_dependency_values,
-                        }) => {
-                            let internal_dependency_values =
-                                populate_internal_dependencies(&external_dependency_values);
-                            match &field_plan.field_type.resolver.carver_or_populator {
-                                CarverOrPopulator::Populator(populator) => {
-                                    let mut populated = ExternalDependencyValues::default();
-                                    populator.populate(
-                                        &mut populated,
-                                        &external_dependency_values,
-                                        &internal_dependency_values,
-                                    );
-                                    ResponseValueOrInProgress::InProgressRecursing(
-                                        InProgressRecursing::new(
-                                            field_plan,
-                                            populated,
-                                            fields_in_progress_new(
-                                                field_plan.selection_set.as_ref().unwrap(),
-                                            ),
-                                        ),
-                                    )
-                                }
-                                CarverOrPopulator::Carver(carver) => {
-                                    ResponseValueOrInProgress::ResponseValue(carver.carve(
-                                        &external_dependency_values,
-                                        &internal_dependency_values,
-                                    ))
-                                }
-                            }
+                        CarverOrPopulator::Carver(carver) => {
+                            ResponseValueOrInProgress::ResponseValue(
+                                carver.carve(
+                                    &external_dependency_values,
+                                    &internal_dependency_values,
+                                ),
+                            )
                         }
-                        ResponseValueOrInProgress::InProgressRecursing(InProgressRecursing {
-                            field_plan,
-                            populated,
-                            selection,
-                        }) => {
-                            unimplemented!()
-                        }
-                    },
-                )
+                    }
+                }
+                ResponseValueOrInProgress::InProgressRecursing(InProgressRecursing {
+                    field_plan,
+                    populated,
+                    selection,
+                }) => {
+                    unimplemented!()
+                }
             },
-        )),
-    )
+        );
+    }
+
+    (false, progressed)
 }
 
-fn populate_internal_dependencies(
+async fn populate_internal_dependencies(
+    field_plan: &FieldPlan<'_>,
     external_dependency_values: &ExternalDependencyValues,
 ) -> InternalDependencyValues {
-    unimplemented!()
+    let mut ret = InternalDependencyValues::new();
+    for internal_dependency in field_plan.field_type.resolver.internal_dependencies.iter() {
+        ret.insert(
+            internal_dependency.name.clone(),
+            match &internal_dependency.resolver {
+                InternalDependencyResolver::ColumnGetter(column_getter) => {
+                    unimplemented!()
+                }
+                _ => unimplemented!(),
+            },
+        )
+        .unwrap();
+    }
+
+    ret
 }
