1ef2eabb7c04b47b15a7e2b1d89f18f874b8aa06
Julian Rosse <julian@helixbass.net>
2025-11-27 16:35:16 -0500

    working on positions

--- src/positions/mod.rs
+++ src/positions/mod.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Debug, ops::Deref};
+use std::{cell::RefCell, fmt::Debug, ops::Deref};
 
 pub struct CharsEmitter<TIterator: Iterator<Item = char>> {
     inner: TIterator,
@@ -21,23 +21,59 @@ impl<TIterator: Iterator<Item = char>> Iterator for CharsEmitter<TIterator> {
 }
 
 #[derive(Debug, Default)]
-pub struct PositionsTracker {}
+pub struct PositionsTracker {
+    last_char: RefCell<Option<Location>>,
+    last_token_start: RefCell<Option<Location>>,
+    just_saw_carriage_return: RefCell<bool>,
+    just_newlined: RefCell<bool>,
+    should_next_char_record_as_token_start: RefCell<bool>,
+    operation_name_positions: RefCell<Vec<Location>>,
+}
 
 impl PositionsTracker {
     pub fn receive_char(&self, ch: char) {
-        unimplemented!()
+        let just_newlined = { *self.just_newlined.borrow() };
+        match ch {
+            '\r' => {
+                *self.just_newlined.borrow_mut() = true;
+                *self.just_saw_carriage_return.borrow_mut() = true;
+            }
+            '\n' => {
+                if !*self.just_saw_carriage_return.borrow() {
+                    *self.just_newlined.borrow_mut() = true;
+                }
+            }
+            _ => {}
+        }
+        let last_char = { *self.last_char.borrow() };
+        let new_last_char = match last_char {
+            Some(last_char) => {
+                if just_newlined {
+                    Location::new(last_char.line + 1, 1)
+                } else {
+                    Location::new(last_char.line, last_char.column + 1)
+                }
+            }
+            None => Location::new(1, 1),
+        };
+        if *self.should_next_char_record_as_token_start.borrow() {
+            *self.last_token_start.borrow_mut() = Some(new_last_char);
+        }
+        *self.last_char.borrow_mut() = Some(new_last_char);
     }
 
     pub fn receive_operation_name(&self) {
-        unimplemented!()
+        self.operation_name_positions
+            .borrow_mut()
+            .push(self.last_token_start.borrow().clone().unwrap());
     }
 
     pub fn receive_token_pre_start(&self) {
-        unimplemented!()
+        *self.should_next_char_record_as_token_start.borrow_mut() = true;
     }
 
     pub fn nth_named_operation_name_location(&self, index: usize) -> Location {
-        unimplemented!()
+        self.operation_name_positions.borrow()[index]
     }
 
     pub fn current() -> Option<impl Deref<Target = Self> + Debug + 'static> {
@@ -63,6 +99,7 @@ impl PositionsTracker {
     }
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct Location {
     /// 1-based
     pub line: usize,
