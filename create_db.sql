58afde5b0f60121e16d691bf9b935e5ac61df2c5
Julian Rosse <julian@helixbass.net>
2025-12-06 12:07:10 -0500

    working on optimization

--- create_db.sql
+++ create_db.sql
@@ -12,10 +12,11 @@ CREATE TABLE actors (
   name TEXT NOT NULL,
   expression TEXT NOT NULL,
   favorite_actor_or_designer_type TEXT NOT NULL,
-  favorite_actor_or_designer_id INT NOT NULL
+  favorite_actor_or_designer_id INT NOT NULL,
+  favorite_designer_id INT NOT NULL
 );
 
-INSERT INTO actors (name, expression, favorite_actor_or_designer_type, favorite_actor_or_designer_id) VALUES
-  ('Katie Cassidy', 'no Serena you can''t have the key', 'designers', (SELECT id FROM designers WHERE name = 'Proenza Schouler'));
-INSERT INTO actors (name, expression, favorite_actor_or_designer_type, favorite_actor_or_designer_id) VALUES
-  ('Jessica Szohr', 'Dan where did you go I don''t like you', 'actors', (SELECT id FROM actors WHERE name = 'Katie Cassidy'));
+INSERT INTO actors (name, expression, favorite_actor_or_designer_type, favorite_actor_or_designer_id, favorite_designer_id) VALUES
+  ('Katie Cassidy', 'no Serena you can''t have the key', 'designers', (SELECT id FROM designers WHERE name = 'Proenza Schouler'), (SELECT id FROM designers WHERE name = 'Proenza Schouler'));
+INSERT INTO actors (name, expression, favorite_actor_or_designer_type, favorite_actor_or_designer_id, favorite_designer_id) VALUES
+  ('Jessica Szohr', 'Dan where did you go I don''t like you', 'actors', (SELECT id FROM actors WHERE name = 'Katie Cassidy'), (SELECT id FROM designers WHERE name = 'Ralph Lauren'));
