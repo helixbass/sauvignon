2036d1eab12f4f03b02c715b9ee8b86c687c3bd2
Julian Rosse <julian@helixbass.net>
2025-12-09 12:44:08 -0500

    working on macros

--- tests/macros.rs
+++ tests/macros.rs
@@ -1,7 +1,19 @@
-use sauvignon::schema;
+use sauvignon::{json_from_response, schema, Schema};
 
-#[test]
-fn test_column_getter() {
+mod shared;
+
+pub use shared::get_schema;
+use shared::{get_db_pool, pretty_print_json};
+
+async fn request_test(schema: &Schema, request: &str, expected: &str) {
+    let db_pool = get_db_pool().await.unwrap();
+    let response = schema.request(request, &db_pool).await;
+    let json = json_from_response(&response);
+    assert_eq!(pretty_print_json(&json), pretty_print_json(expected));
+}
+
+#[tokio::test]
+async fn test_column_getter() {
     let schema = schema! {
         types => [
             Actor => {
@@ -19,4 +31,25 @@ fn test_column_getter() {
             }
         ]
     };
+
+    request_test(
+        &schema,
+        r#"
+            query {
+              actorKatie {
+                name
+              }
+            }
+        "#,
+        r#"
+            {
+              "data": {
+                "actorKatie": {
+                  "name": "Katie Cassidy"
+                }
+              }
+            }
+        "#,
+    )
+    .await;
 }
