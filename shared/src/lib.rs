e6a16705ec1188b631bc1f9c04dd0671f9a63cc0
Julian Rosse <julian@helixbass.net>
2025-12-12 17:53:07 -0500

    working on plural

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,6 +1,7 @@
 pub fn pluralize(value: &str) -> String {
     match value {
         "Species" => "Species".to_owned(),
+        "species" => "species".to_owned(),
         value => format!("{value}s"),
     }
 }
