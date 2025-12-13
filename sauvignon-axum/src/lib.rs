6e14dc207ff8526cb5e7821cd7c8aa965c584072
Julian Rosse <julian@helixbass.net>
2025-12-13 12:05:11 -0500

    working on axum

--- sauvignon-axum/src/lib.rs
+++ sauvignon-axum/src/lib.rs
@@ -1,1 +1,13 @@
 pub use axum;
+
+use axum::Extension;
+use sauvignon::Schema;
+use sqlx::{Pool, Postgres};
+
+pub async fn graphql(
+    Extension(schema): Extension<Schema>,
+    Extension(db_pool): Extension<Pool<Postgres>>,
+    GraphQLRequest(request): GraphQLRequest,
+) -> GraphQLResponse {
+    GraphQLResponse(schema.request(request, &db_pool))
+}
