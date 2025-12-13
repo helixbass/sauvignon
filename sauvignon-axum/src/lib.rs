8e691406cc432ca2d6c8074fc0e16cefbb46c32f
Julian Rosse <julian@helixbass.net>
2025-12-13 12:26:07 -0500

    working on axum

--- sauvignon-axum/src/lib.rs
+++ sauvignon-axum/src/lib.rs
@@ -1,7 +1,13 @@
 pub use axum;
 
-use axum::Extension;
+use axum::{
+    extract::{FromRequest, Request},
+    http::StatusCode,
+    response::{IntoResponse as _, Response},
+    Extension, Json,
+};
 use sauvignon::Schema;
+use serde::Deserialize;
 use sqlx::{Pool, Postgres};
 
 pub async fn graphql(
@@ -9,5 +15,28 @@ pub async fn graphql(
     Extension(db_pool): Extension<Pool<Postgres>>,
     GraphQLRequest(request): GraphQLRequest,
 ) -> GraphQLResponse {
-    GraphQLResponse(schema.request(request, &db_pool))
+    GraphQLResponse(schema.request(&request.query, &db_pool))
+}
+
+pub struct GraphQLRequest(RequestFields);
+
+impl<TState> FromRequest<TState> for GraphQLRequest
+where
+    TState: Send + Sync,
+{
+    type Rejection = Response;
+
+    async fn from_request(request: Request, state: &TState) -> Result<Self, Self::Rejection> {
+        Json::<RequestFields>::from_request(request, state)
+            .await
+            .map(|json| Self(json.0))
+            .map_err(|err| {
+                (StatusCode::BAD_REQUEST, format!("Invalid JSON body: {err}")).into_response()
+            })
+    }
+}
+
+#[derive(Deserialize)]
+pub struct RequestFields {
+    pub query: String,
 }
